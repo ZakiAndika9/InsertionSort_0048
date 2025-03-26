5076951b7ac676545bec2f78977cb205ed87974b
ZakiAndika9 <m.zaki.andika9@gmail.com>
2025-03-26 13:20:16 +0700

    commit 4

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -24,4 +24,22 @@ cout << endl;
 cout << "======================" << endl;
 cout << "Masukkan Element Array" << endl;
 cout << "======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+cout << "Data ke-" << (i + 1) << ": ";
+cin >> arr [i];
+}
+}
+
+void insertSort()
+{
+
+int temp;
+int j, i;
+
+for (i =1; i <= n - 1; i++)
+{
+
+}
 }
